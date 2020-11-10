79f47991f7d8f3c37691a911a1180d9ca256b55d
Kate Foreman <ktfrmn@Kates-MacBook-Pro.local>
2020-11-10 09:59:39 +0000

    pushing changes to task

--- task_2/data_cleaning_scripts/cleaning_data .R
+++ task_2/data_cleaning_scripts/cleaning_data .R
@@ -8,24 +8,23 @@ library(here)
 cake_ingredients_data <- read_csv(here("raw_data/cake-ingredients-1961.csv")) 
 cake_abbreviations_data <- read_csv(here("raw_data/cake_ingredient_code.csv"))
 glimpse(cake_ingredients_data) 
-head(cake_ingredients_data)
-
-cake_ingredients_data %>% 
-  slice_max(CA, n = 1)
+head(cake_ingredients_data) 
 
 #Pivoting the data to be longer 
 
 long_cake_data <- cake_ingredients_data %>% 
-  pivot_longer(cols = c(AE, BM, BP, BR, BS, CA, CC, CE, CI, CS, CT, DC, EG, EW, EY, FR, GN, HC, LJ, LR, MK, 
-               NG, NS, RM, SA, SC, SG, SR, SS, ST, VE, WR, YT, ZH), names_to = "ingredients", values_to = 
-                 "quantities") 
+  pivot_longer(cols = c(AE, BM, BP, BR, BS, CA, CC, CE, CI, CS, CT, 
+                        DC, EG, EW, EY, FR, GN, HC, LJ, LR, MK, 
+                        NG, NS, RM, SA, SC, SG, SR, SS, ST, VE, WR, YT, ZH), 
+                        names_to = "ingredients", 
+                        values_to = "quantities") 
 
 #Imputing the missing values in quantities 
 
 imputed_long_cake_data <- long_cake_data %>%
   mutate(quantities = coalesce(quantities, 0)) 
 
-#Doing a left join on the data 
+#Doing a left join to join the second dataset 
 
 joined_data <- imputed_long_cake_data %>% 
   left_join(cake_abbreviations_data, by = c("ingredients" = "code")) 
@@ -35,7 +34,7 @@ joined_data <- imputed_long_cake_data %>%
 joined_data_abbreviations_dropped <- joined_data %>% 
   select(-ingredients) 
 
-#Imputing missing values in measure 
+#Imputing missing values in measure to cup 
 
 imputed_data <- joined_data_abbreviations_dropped %>% 
   mutate(measure = coalesce(measure, "cup"))
@@ -43,3 +42,5 @@ imputed_data <- joined_data_abbreviations_dropped %>%
 #Cleaning names 
   
 imputed_clean_data <- clean_names(imputed_data) 
+
+write_csv(imputed_clean_data, "clean_cake_data.csv") 
